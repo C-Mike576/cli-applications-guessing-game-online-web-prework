dbc278e7185293100a059f97e3cbe4976ceda80c
Michael Calkins <m.calkins576@gmail.com>
2019-10-25 19:55:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,19 @@
 # Code your solution here!
+def my_num
+  rand(1..6)
+end
+
+def prompt_user
+  puts "What number am I thinking of between 1 and 6, or type 'exit' to quit"
+end
+
+def get_input
+  gets.chomp
+end
+
+def run_
+  pc_nummber = my_num
+  
+  
+
+end
